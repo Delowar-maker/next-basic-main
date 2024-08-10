506acf1e3d6247be103652c7e6fe3bd0cdbed580
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-08-10 15:15:42 +0600

    Dynamic params

---
+++ next-app/app/blogs/[id]/page.js
@@ -0,0 +1,16 @@
+import blogs from "@/app/data/blogs";
+
+
+export default function page({ params }) {
+    const { id } = params; // folder name id ty hoba 
+    const blog = blogs.find(blog => blog.id === id);
+    const { title, discription } = blog;
+    return (
+        <div>
+            <div className="p-8">
+                <h1 className="text-xl font-bold">{title}</h1>
+                <p className="mt-6">{discription}</p>
+            </div>
+        </div>
+    )
+}
