167e0a425a9a638f47a54762196d3d7fe8d21502
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-08-10 15:19:44 +0600

    Dynamic blogs id

--- next-app/app/blogs/page.js
+++ next-app/app/blogs/page.js
@@ -1,13 +1,18 @@
+import Link from "next/link";
 import BlogItem from "../components/BlogItem";
 import blogs from "../data/blogs";
 
 export default function Blogs() {
     return (
-        <div className='p-8'>
-            <h1 className='text-xl font-bold'>Blog List</h1>
+        <div className="p-8">
+            <h1 className="text-xl font-bold">Blog List</h1>
             <div className="mt-6 p4">
-                {blogs.map(blog => <BlogItem title={blog.title} discription={blog.discription} key={blog.id} />)}
+                {blogs.map((blog) => (
+                    <Link href={`/blogs/${blog.id}`} key={blog.id}>
+                        <BlogItem title={blog.title} discription={blog.discription} />
+                    </Link>
+                ))}
             </div>
         </div>
-    )
+    );
 }
