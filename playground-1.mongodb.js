7896dc1fce46046b80cb286fc7832ac5f28e390e
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2025-01-04 18:26:40 +0600

    Using map

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -411,6 +411,13 @@ exports.CreateToken=(req,res)=>{
     let Token= jwt.sign(Payload,"SecretKey123");
 
     res.send(Token)
+     array1 = [1, 4, 9, 16];
+
+// Pass a function to map
+const map1 = array1.map((x) => x * 2);
+
+console.log(map1);
+// Expected output: Array [2, 8, 18, 32]
 
 
     db.employees.aggregate([
