54e3ca6c8c04c71b3616ea579856c21c00d99d57
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2025-01-15 23:36:30 +0600

    Dom

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -519,3 +519,7 @@ console.log(shopping);
     </script>
 </body>
 </html>
+const paragraphs = document.querySelectorAll("p");
+// paragraphs[0] is the first <p> element
+// paragraphs[1] is the second <p> element, etc.
+alert(paragraphs[0].nodeName);
