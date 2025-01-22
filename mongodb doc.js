ff4f0252d2296307f89d11e53af80696088a21da
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2025-01-22 23:08:59 +0600

    call sack

--- mongodb doc.js
+++ mongodb doc.js
@@ -130,3 +130,16 @@ function mysticalAnimal(type, collects, canFly){
     this.collects = collects;
     this.canFly = canFly;
 }
+function greeting() {
+  // [1] Some code here
+  sayHi();
+  // [2] Some code here
+}
+function sayHi() {
+  return "Hi!";
+}
+
+// Invoke the `greeting` function
+greeting();
+
+// [3] Some code here
