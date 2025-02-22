8e68736e27e54ab1200ba0b9b69890bc703788c3
Md.Delowar Hossain <delowarhossain1996@gmail.com>
2025-02-22 23:44:08 +0600

    node start

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -614,3 +614,11 @@ let person = {
   name: 'John',
   age: 25,
 };
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(3000);
+console.log('Example app listening on port 3000!');
+
