5c0eb63cd4104890c6f05e7b030cb5653792fb15
Md.Delowar Hossain <delowarhossain1996@gmail.com>
2025-02-25 23:21:14 +0600

    reduce

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -632,3 +632,16 @@ db.employees.aggregate([
 
 var jwt = require('jsonwebtoken');
 var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);
+??
